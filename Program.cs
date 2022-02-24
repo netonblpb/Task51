52b5aa4c8fba4884c098df4f7e9efa1d80343fc5
netonblpb <skullbog@gmail.com>
2022-02-24 13:11:47 +0300

    Task51 complete

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//  Задать двумерный массив следующим правилом: Aₘₙ = m+n.
+
+Console.Clear();
+Console.Write("Задайте размер по Х: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Задайте размер по Y: ");
+int n = int.Parse(Console.ReadLine());
+int[,] array = new int[m,n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = i + j;
+        Console.Write($"{array[i, j]}\t");
+    }
+    Console.WriteLine();
+}
